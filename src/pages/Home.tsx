1595799d35c90d1fa0305b30bfd2f4e9c0ba56bf
ryan <ryan.patel21@imperial.ac.uk>
2023-08-19 19:09:58 +0100

    Added icon and heading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
 import getStyles from "../Style";
+import mom from "../assets/images/mom.svg";
 
 function Home() {
-  return <div className={getStyles(styles, "ctn")}></div>;
+  return (
+    <div className={getStyles(styles, "ctn")}>
+      <img src={mom} className={getStyles(styles, "momIcon")} alt="mom" />
+      <p className={getStyles(styles, "header")}>Hey, how are you feeling?</p>
+    </div>
+  );
 }
 
 const styles = {
-  ctn: ["bg-paper", "w-full", "h-full"],
+  ctn: [
+    "bg-paper",
+    "flex",
+    "flex-col",
+    "justify-center",
+    "items-center",
+    "gap-[25px]",
+    "sm:gap-[55px]",
+    "w-full",
+    "h-full",
+  ],
+  momIcon: ["h-[180px]"],
+  header: ["text-xl", "font-medium"],
 };
 
 export default Home;
